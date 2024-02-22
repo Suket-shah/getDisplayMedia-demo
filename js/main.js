749bd62e0c71326992fd3c93547b49cb80c85d51
Suket Shah <suket.shah@utexas.edu>
2024-02-22 11:16:27 -0500

    Screen recording works in chrome not in firefox.

--- js/main.js
+++ js/main.js
@@ -33,60 +33,61 @@ function onShareScreen() {
       navigator.mediaDevices.getDisplayMedia(constraints).then(function(screenStream) {
           //check for microphone
           navigator.mediaDevices.enumerateDevices().then(function(devices) {
-              devices.forEach(function(device) {
-                if (device.kind == "audioinput") {
-                  micNumber++;
-                }
-              });
+              // devices.forEach(function(device) {
+              //   if (device.kind == "audioinput") {
+              //     micNumber++;
+              //   }
+              // });
 
-              if (micNumber == 0) {
-                getStreamSuccess(screenStream);
-              } else {
-                navigator.mediaDevices.getUserMedia({audio: true}).then(function(micStream) {
-                    var composedStream = new MediaStream();
+              getStreamSuccess(screenStream);
+              // if (micNumber == 0) {
+              //   getStreamSuccess(screenStream);
+              // } else {
+              //   navigator.mediaDevices.getUserMedia({audio: true}).then(function(micStream) {
+              //       var composedStream = new MediaStream();
 
-                    //added the video stream from the screen
-                    screenStream.getVideoTracks().forEach(function(videoTrack) {
-                      composedStream.addTrack(videoTrack);
-                    });
+              //       //added the video stream from the screen
+              //       screenStream.getVideoTracks().forEach(function(videoTrack) {
+              //         composedStream.addTrack(videoTrack);
+              //       });
 
-                    //if system audio has been shared
-                    if (screenStream.getAudioTracks().length > 0) {
-                      //merge the system audio with the mic audio
-                      var context = new AudioContext();
-                      var audioDestination = context.createMediaStreamDestination();
+              //       //if system audio has been shared
+              //       if (screenStream.getAudioTracks().length > 0) {
+              //         //merge the system audio with the mic audio
+              //         var context = new AudioContext();
+              //         var audioDestination = context.createMediaStreamDestination();
 
-                      const systemSource = context.createMediaStreamSource(screenStream);
-                      const systemGain = context.createGain();
-                      systemGain.gain.value = 1.0;
-                      systemSource.connect(systemGain).connect(audioDestination);
-                      console.log("added system audio");
+              //         const systemSource = context.createMediaStreamSource(screenStream);
+              //         const systemGain = context.createGain();
+              //         systemGain.gain.value = 1.0;
+              //         systemSource.connect(systemGain).connect(audioDestination);
+              //         console.log("added system audio");
 
-                      if (micStream && micStream.getAudioTracks().length > 0) {
-                        const micSource = context.createMediaStreamSource(micStream);
-                        const micGain = context.createGain();
-                        micGain.gain.value = 1.0;
-                        micSource.connect(micGain).connect(audioDestination);
-                        console.log("added mic audio");
-                      }
+              //         if (micStream && micStream.getAudioTracks().length > 0) {
+              //           const micSource = context.createMediaStreamSource(micStream);
+              //           const micGain = context.createGain();
+              //           micGain.gain.value = 1.0;
+              //           micSource.connect(micGain).connect(audioDestination);
+              //           console.log("added mic audio");
+              //         }
 
-                      audioDestination.stream.getAudioTracks().forEach(function(audioTrack) {
-                          composedStream.addTrack(audioTrack);
-                        });
-                    } else {
-                      //add just the mic audio
-                      micStream.getAudioTracks().forEach(function(micTrack) {
-                        composedStream.addTrack(micTrack);
-                      });
-                    }
+              //         audioDestination.stream.getAudioTracks().forEach(function(audioTrack) {
+              //             composedStream.addTrack(audioTrack);
+              //           });
+              //       } else {
+              //         //add just the mic audio
+              //         micStream.getAudioTracks().forEach(function(micTrack) {
+              //           composedStream.addTrack(micTrack);
+              //         });
+              //       }
                     
-                  getStreamSuccess(composedStream);
+              //     getStreamSuccess(composedStream);
                   
-                  })
-                  .catch(function(err) {
-                    log("navigator.getUserMedia error: " + err);
-                  });
-              }
+              //     })
+              //     .catch(function(err) {
+              //       log("navigator.getUserMedia error: " + err);
+              //     });
+              // }
             })
             .catch(function(err) {
               log(err.name + ": " + err.message);
@@ -102,11 +103,11 @@ function onShareScreen() {
 function getStreamSuccess(stream) {
   localStream = stream;
   localStream.getTracks().forEach(function(track) {
-    if (track.kind == "audio") {
-      track.onended = function(event) {
-        log("audio track.onended Audio track.readyState=" + track.readyState + ", track.muted=" + track.muted);
-      };
-    }
+    // if (track.kind == "audio") {
+    //   track.onended = function(event) {
+    //     log("audio track.onended Audio track.readyState=" + track.readyState + ", track.muted=" + track.muted);
+    //   };
+    // }
     if (track.kind == "video") {
       track.onended = function(event) {
         log("video track.onended Audio track.readyState=" + track.readyState + ", track.muted=" + track.muted);
@@ -120,20 +121,20 @@ function getStreamSuccess(stream) {
   recBtn.disabled = false;
   shareBtn.disabled = true;
 
-  try {
-    window.AudioContext = window.AudioContext || window.webkitAudioContext;
-    window.audioContext = new AudioContext();
-  } catch (e) {
-    log("Web Audio API not supported.");
-  }
+  // try {
+  //   window.AudioContext = window.AudioContext || window.webkitAudioContext;
+  //   window.audioContext = new AudioContext();
+  // } catch (e) {
+  //   log("Web Audio API not supported.");
+  // }
 
-  soundMeter = window.soundMeter = new SoundMeter(window.audioContext);
-  soundMeter.connectToSource(localStream, function(e) {
-    if (e) {
-      log(e);
-      return;
-    }
-  });
+  // soundMeter = window.soundMeter = new SoundMeter(window.audioContext);
+  // soundMeter.connectToSource(localStream, function(e) {
+  //   if (e) {
+  //     log(e);
+  //     return;
+  //   }
+  // });
 }
 
 function onBtnRecordClicked() {
@@ -161,6 +162,7 @@ function onBtnRecordClicked() {
     }
 
     mediaRecorder.ondataavailable = function(e) {
+      log("data available");
       chunks.push(e.data);
     };
 
@@ -245,53 +247,53 @@ function log(message) {
   console.log(message);
 }
 
-// Meter class that generates a number correlated to audio volume.
-// The meter class itself displays nothing, but it makes the
-// instantaneous and time-decaying volumes available for inspection.
-// It also reports on the fraction of samples that were at or near
-// the top of the measurement range.
-function SoundMeter(context) {
-  this.context = context;
-  this.instant = 0.0;
-  this.slow = 0.0;
-  this.clip = 0.0;
-  this.script = context.createScriptProcessor(2048, 1, 1);
-  var that = this;
-  this.script.onaudioprocess = function(event) {
-    var input = event.inputBuffer.getChannelData(0);
-    var i;
-    var sum = 0.0;
-    var clipcount = 0;
-    for (i = 0; i < input.length; ++i) {
-      sum += input[i] * input[i];
-      if (Math.abs(input[i]) > 0.99) {
-        clipcount += 1;
-      }
-    }
-    that.instant = Math.sqrt(sum / input.length);
-    that.slow = 0.95 * that.slow + 0.05 * that.instant;
-    that.clip = clipcount / input.length;
-  };
-}
+// // Meter class that generates a number correlated to audio volume.
+// // The meter class itself displays nothing, but it makes the
+// // instantaneous and time-decaying volumes available for inspection.
+// // It also reports on the fraction of samples that were at or near
+// // the top of the measurement range.
+// function SoundMeter(context) {
+//   this.context = context;
+//   this.instant = 0.0;
+//   this.slow = 0.0;
+//   this.clip = 0.0;
+//   this.script = context.createScriptProcessor(2048, 1, 1);
+//   var that = this;
+//   this.script.onaudioprocess = function(event) {
+//     var input = event.inputBuffer.getChannelData(0);
+//     var i;
+//     var sum = 0.0;
+//     var clipcount = 0;
+//     for (i = 0; i < input.length; ++i) {
+//       sum += input[i] * input[i];
+//       if (Math.abs(input[i]) > 0.99) {
+//         clipcount += 1;
+//       }
+//     }
+//     that.instant = Math.sqrt(sum / input.length);
+//     that.slow = 0.95 * that.slow + 0.05 * that.instant;
+//     that.clip = clipcount / input.length;
+//   };
+// }
 
-SoundMeter.prototype.connectToSource = function(stream, callback) {
-  console.log("SoundMeter connecting");
-  try {
-    this.mic = this.context.createMediaStreamSource(stream);
-    this.mic.connect(this.script);
-    // necessary to make sample run, but should not be.
-    this.script.connect(this.context.destination);
-    if (typeof callback !== "undefined") {
-      callback(null);
-    }
-  } catch (e) {
-    console.error(e);
-    if (typeof callback !== "undefined") {
-      callback(e);
-    }
-  }
-};
-SoundMeter.prototype.stop = function() {
-  this.mic.disconnect();
-  this.script.disconnect();
-};
+// SoundMeter.prototype.connectToSource = function(stream, callback) {
+//   console.log("SoundMeter connecting");
+//   try {
+//     this.mic = this.context.createMediaStreamSource(stream);
+//     this.mic.connect(this.script);
+//     // necessary to make sample run, but should not be.
+//     this.script.connect(this.context.destination);
+//     if (typeof callback !== "undefined") {
+//       callback(null);
+//     }
+//   } catch (e) {
+//     console.error(e);
+//     if (typeof callback !== "undefined") {
+//       callback(e);
+//     }
+//   }
+// };
+// SoundMeter.prototype.stop = function() {
+//   this.mic.disconnect();
+//   this.script.disconnect();
+// };
